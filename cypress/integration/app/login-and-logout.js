cbdb9de5cb81f81147d1bbc6b1fc18492a25f2a4
ade.ajayi <ade.ajayi@sainsbury.co.uk>
2019-05-29 10:06:36 +0100

    iframe issues

---
+++ cypress/integration/app/login-and-logout.js
@@ -0,0 +1,26 @@
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
+
+
+
+
+describe('Login and logout on serviceNow', () => {
+    it('Visits the vehicle checks site', () => {
+      cy.visit('https://sainsburystest.service-now.com/login.do')
+
+
+      cy.get('#user_name').type('Ade.Test')
+      cy.get('#user_password').type('Pie')
+      cy.get('#sysverb_login').click()
+    })
+})
+
+
+
+
+
