f5aefe6470cdd016acf41c7042b6273dca1552b6
ade.ajayi <ade.ajayi@sainsbury.co.uk>
2020-01-13 15:54:39 +0000

    update

--- cypress/integration/app/login-and-logout.js
+++ cypress/integration/app/login-and-logout.js
@@ -1,16 +1,16 @@
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   // returning false here prevents Cypress from
+//   // failing the test
+//   return false
+// })
 
-describe('Login and logout on serviceNow', () => {
-    it('Visits the serviceNow site', () => {
-      cy.visit('https://sainsburystest.service-now.com/login.do')
-      cy.get('#user_name').type('Ade.Test')
-      cy.get('#user_password').type('Pie')
-      cy.get('#sysverb_login').click()
+describe('Fill Form', () => {
+    it('Visit the web', () => {
+      cy.visit('https://ultimateqa.com/filling-out-forms/')
+      cy.get('#et_pb_contact_name_0').type('This is a test')
+      cy.get('#et_pb_contact_message_0').type('Automation testing')
+      cy.get('#et_pb_contact_form_0 > .et_pb_contact > .et_pb_contact_form > .et_contact_bottom_container > .et_pb_contact_submit').click()
     })
     it('Login to serviceNow', () => {
       cy.get('#user_name').type('Ade.Test')
